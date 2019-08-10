59893f418e5c8c5c12ca2b54e2e06ca5215c7420
Lorenzo <lorenzo.daneo91@gmail.com>
2019-08-10 13:55:15 +0200

    nbproject

---
+++ BTPic24.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BTPic24.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BTPic24.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=btpic24.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/btpic24.x.tar
